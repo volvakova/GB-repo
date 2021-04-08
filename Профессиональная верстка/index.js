21fa4581742b230d0fc99a45d79374f2d0087d6f
IrinaMelnikova <ira.volvakova@yandex.ru>
2021-04-08 12:16:32 +0300

    Доработана верстка

--- Профессиональная верстка/index.js
+++ Профессиональная верстка/index.js
@@ -3,7 +3,8 @@ const burger = document.querySelector('.burger')
 const closebtn = document.querySelector('.menu__close')
 const overllay = document.querySelector('.overllay')
 const menu = document.querySelector('.menu')
-
+const filterbut = document.querySelector('.filter__but')
+const filteroverllay = document.querySelector('.filter__overllay')
 
 let isOpen = false;
 
@@ -23,3 +24,15 @@ burger.addEventListener('click', () => {
             overllay.classList.remove('open')
             document.body.style.overflow = 'auto'
         });
+
+filterbut.addEventListener('click', () => {
+            isOpen = !isOpen
+            filteroverllay.classList.toggle('open')
+        if(isOpen){
+            document.body.style.overflow = 'hidden'
+        }
+        else{
+            document.body.style.overflow = 'auto'
+        }  
+
+})
